0b234741d79780f2c0862c128d8bcd25ab3bde63
ding-sol <dingsol503@gmail.com>
2022-07-06 13:45:57 +0900

    first commit

---
+++ algorithm/example.js
@@ -0,0 +1,12 @@
+//반복문 사용//
+let answer = 0;
+const limit = 100;
+
+for( let i=1; i<limit; i=i+2) {
+    answer = answer + 1;
+}
+
+//함수 사용//
+const limit = 100;
+
+const answer = Math.floor(limit/2)
